5af2f7bc22d96e1db6bc42a476fdbc67d09d5171
JoshHumpherey <jhumphrey321@gmail.com>
2017-03-06 23:25:28 -0500

    Version 1.1
    Fixed Encoder function and various bugs w/ the ASCII ref numbers and values

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,10 +2,10 @@
 # Written by Josh Humphrey
 # Based on project from: https://inventwithpython.com/chapter14.html
 
-def Mode():
+def Mode(Mode):
     print("Do you want to transmit or recieve a secret message?")
     print("Enter '1' for transmission or '0' for recieving")
-    Mode = input()
+    Mode = int(input())
     if Mode == 1:
         print("You have chosent to 'Transmit' a message")
         return Mode
@@ -16,7 +16,7 @@ def Mode():
         print("Sorry! You did not choose a correct mode :(")
         exit()
 
-def GetKey():
+def GetKey(Key):
     print("Please enter the value of your secret key!")
     print("Keep in mind it must be an integer between 0 and 25")
     Key = int(input())
@@ -32,13 +32,20 @@ def Message():
     Message = input()
     return Message
 
-def Encoder():
+def Encoder(Message):
     length = len(Message)
+    EncryptedASCII = list(range(0,length))
+    EncryptedMessage = EncryptedASCII
     i = 0
     for i in range(length):
-        EncryptedMessage[i] = ord(Message[i]) + Key
-        i += 1
-    return EncryptedMessage
+        if Message[i] == ' ':
+            EncryptedMessage[i]=Message[i]
+        else:
+            EncryptedASCII[i] = ord(Message[i]) + 5
+            EncryptedMessage[i] = chr(EncryptedASCII[i])
+            i += 1
+    result = "".join(EncryptedMessage)
+    return result
 
 def Decoder():
     length = len(Message)
@@ -48,18 +55,11 @@ def Decoder():
         i += 1
     return DecryptedMessage
 
-#automated test script for parameters
-auto = open("test.txt","r")
-automode = auto.read(1)
-autokey = auto.read(2)
-#automated test script for message
-hellomessage = open("message.txt","r")
-automessage = hellomessage.read(20)
 
 # Main Function
-Type = Mode(automode)
-Key = GetKey(autokey)
-Message = Message(automessage)
+Type = Mode(Mode)
+Key = GetKey(Key)
+Message = Message(Message)
 if Type == 1:
    Encoder(Message)
    print(Message)
@@ -67,3 +67,6 @@ if Type == 1:
 else:
    Decoder(Message)
    print(Message)
+
+
+    
